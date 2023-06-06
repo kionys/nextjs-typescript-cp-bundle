b7cbeaf65afc0ba77b11c331f03552dd20ac2709
kionys <kiwon7271@naver.com>
2023-06-07 04:14:36 +0900

    test: git 설정 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import styled from '@emotion/styled'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import styled from "@emotion/styled";
 
 const Style = {
-  Container : styled.div`
-  border: 1px solid;
-  background: white;
-  text-align: center
-  `
-}
+  Container: styled.div`
+    border: 1px solid;
+    background: white;
+    text-align: center;
+  `,
+};
 const Home: NextPage = () => {
-  return (
-     <Style.Container>컴포넌트</Style.Container>
-  )
-}
+  return <Style.Container>테스트 컴포넌트</Style.Container>;
+};
 
-export default Home
+export default Home;
